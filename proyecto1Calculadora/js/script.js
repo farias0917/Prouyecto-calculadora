2618cbd363af9619b032103b9561a045eeda9b7b
farias0917 <128154875+farias0917@users.noreply.github.com>
2023-10-09 11:56:37 -0500

    Add files via upload

---
+++ proyecto1Calculadora/js/script.js
@@ -0,0 +1,44 @@
+const pantalla = document.querySelector(".pantalla")
+const botones = document.querySelectorAll(".boton")
+
+botones.forEach(boton => {
+    boton.addEventListener("click", ()=>{
+        const botonOn = boton.textContent
+
+        if (boton.id === "borrar") {
+            if (pantalla.textContent.length === 1) {
+                pantalla.textContent = "0";
+            }else{
+                pantalla.textContent = pantalla.textContent.slice(0,-1);
+               
+             }
+              return;
+            
+            }
+
+        if (boton.id === "igual") {
+            try{
+                pantalla.textContent = eval(pantalla.textContent)
+                if (pantalla.textContent === "") {  
+                    pantalla.textContent = "¡Error!"
+                }
+            }catch{
+                pantalla.textContent = "¡Error!"
+             }
+             return
+        }
+
+       
+        
+         if (boton.id === "limpiar") {
+             pantalla.textContent = "0";
+              return;
+            }
+
+            if (pantalla.textContent === "0" || pantalla.textContent === "¡Error!") {
+                pantalla.textContent = botonOn
+            }else{
+                pantalla.textContent += botonOn
+            }        
+    })
+});
